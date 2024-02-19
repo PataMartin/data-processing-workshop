d1135066b3b4309c28296802cc38c6f53bbd4fc4
Martin Infante <martin.infante@patagoniansys.com>
2024-02-19 14:55:20 -0300

    Add event generator

---
+++ event_generator.py
@@ -0,0 +1,33 @@
+import datetime as dt
+import os
+import time
+
+import requests
+
+API_KEY = os.environ["API_KEY"]
+URL = os.environ["URL"]
+
+
+def main():
+    """
+    This function will send events to the log enpoint.
+    """
+    event_id = 0
+
+    header = {"X-API-Key": API_KEY}
+    while event_id < 11:
+        print(f"Sending event {event_id}")
+        payload = {
+            "id": "event_id",
+            "source": "PAYMENTS" if event_id % 2 == 0 else "AUTH",
+            "event_type": 1 if event_id % 2 == 0 else 2,
+            "ts": str(dt.datetime.today),
+        }
+        r = requests.post(URL, json=payload, headers=header)
+        print(f"response: {r.json()}")
+        time.sleep(10)
+        event_id += 1
+
+
+if __name__ == "__main__":
+    main()
